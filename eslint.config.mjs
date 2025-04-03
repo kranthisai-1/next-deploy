df74e6d58972760bf2fce5b592cd97ed0f6cb39d
Kranthi Sai <kranthisai@Kranthis-MacBook-Pro.local>
2025-04-03 23:50:10 +0530

    fix: fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,26 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const prettierOptions = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
+
+const config = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      'prettier/prettier': ['error', prettierOptions],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config;
