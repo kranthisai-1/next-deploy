1e21a861c059b052f2c69e91e0b14270b4f4933c
Kranthi Sai <kranthisai@Kranthis-MacBook-Pro.local>
2025-04-10 11:52:59 +0530

    fix: fixes

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -50,7 +50,6 @@ export default function Signin() {
         <div className="bg-white shadow-lg rounded-lg p-8 w-[550px] h-[430px] mt-[-350px]">
           <div>hello bro</div>
           <div>hello bro</div>
-          <div>hello bro3 </div>
           <div className="logo mt-3 ml-[30%]"></div>
           <div className="flex justify-center mt-[140px] pl-2">
             <GoogleLogin
